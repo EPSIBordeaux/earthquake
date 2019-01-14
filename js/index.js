98fddd2263135310e21d768c491fe2ad073f6067
Sylvain METAYER <contact@sylvainmetayer.fr>
2019-01-14 17:33:43 +0100

    WIP on adding point on earth

--- js/index.js
+++ js/index.js
@@ -3,21 +3,16 @@ const SPEED_CLOUDS = SPEED_EARTH / 2;
 
 const environment = setupScene();
 
-var groupterre = new THREE.Group();
-environment.scene.add(groupterre);
+var earthGroup = new THREE.Group();
+environment.scene.add(earthGroup);
 
-var earth = createSphere(15, 320, 320, groupterre, undefined, false);
-var nuages = createSphere(15.1, 320, 320, groupterre, undefined, false);
+var earth = createSphere(15, 320, 320, earthGroup, undefined, false);
 
 loadTexture("earth_color.jpg", function (texture) {
-    earth.material.map = texture;
+    // earth.material.map = texture;
     earth.material.needsUpdate = true;
-});
-
-loadTexture("earth_cloud.jpg", function (texture) {
-    nuages.material.alphaMap = texture;
-    nuages.material.transparent = true;
-    nuages.material.needsUpdate = true;
+    earth.material.alphaMap = texture;
+    earth.material.transparent = true;
 });
 
 lightSetup(environment.scene, earth);
@@ -26,7 +21,6 @@ var controls = new THREE.OrbitControls(environment.camera);
 
 function animate() {
     earth.rotateY(SPEED_EARTH);
-    nuages.rotateY(SPEED_CLOUDS);
     controls.update();
     environment.renderer.render(environment.scene, environment.camera);
     requestAnimationFrame(animate);
@@ -34,3 +28,30 @@ function animate() {
 
 animate();
 
+var raycaster = new THREE.Raycaster();
+function drawPoint(event) {
+    event.preventDefault();
+    var position = new THREE.Vector2();
+    // On conserve la position de la souris dans l'espace de coordonnées
+    // NDC (Normalized device coordinates).
+    var domRect = environment.renderer.domElement.getBoundingClientRect();
+    position.x = (event.clientX / domRect.width) * 2 - 1 + domRect.left;
+    position.y = - (event.clientY / domRect.height) * 2 + 1 + domRect.top;
+
+    raycaster.setFromCamera(position, environment.camera);
+    var selectionnes = raycaster.intersectObjects([earth]);
+
+    console.log("Intersect result")
+    console.log(selectionnes[0]);
+
+    var pointGeometry = new THREE.Geometry();
+    pointGeometry.vertices.push(position);
+    var pointMaterial = new THREE.PointsMaterial({ color: 0xF00 });
+    var point = new THREE.Points(pointGeometry, pointMaterial);
+    earth.add(point);
+
+    console.log("point")
+    console.log(point);
+}
+
+environment.renderer.domElement.addEventListener('click', drawPoint);
