651f8d76b48c163a83c7421654f64fd725e13d4c
Sylvain METAYER <contact@sylvainmetayer.fr>
2019-01-15 11:58:37 +0100

    poc cone

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ var nuages = createSphere(1.51, 32, 32, groupterre, undefined, false);
 loadTexture("earth_color.jpg", function (texture) {
     earth.material.map = texture;
     earth.material.needsUpdate = true;
+    nuages.material.transparent = true;
 });
 
 loadTexture("earth_cloud.jpg", function (texture) {
@@ -24,12 +25,22 @@ lightSetup(environment.scene, earth);
 
 var controls = new THREE.OrbitControls(environment.camera);
 
-var animate = function() {
-    earth.rotateY(SPEED_EARTH);
-    nuages.rotateY(SPEED_CLOUDS);
+var animate = function () {
+    //earth.rotateY(SPEED_EARTH);
+    //nuages.rotateY(SPEED_CLOUDS);
     controls.update();
     environment.renderer.render(environment.scene, environment.camera);
     requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
 
+function createCone(earth, radius, radialSegments, heightsSegments) {
+    console.log(earth.geometry.parameters);
+    var geometry = new THREE.ConeGeometry(radius, radialSegments, heightsSegments);
+    var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    var cone = new THREE.Mesh(geometry, material);
+    cone.position.add(new THREE.Vector3(earth.geometry.parameters.radius, 0, 0));
+    earth.add(cone);
+}
+
+createCone(earth, .125, .2, .02);
